51805bf1c92b6bf6aab09dff186901539c783147
Craig Campbell <craig.campbell8@gmail.com>
2015-11-07 23:39:02 -0800

    9 of 10 tests working

--- test/router-test.js
+++ test/router-test.js
@@ -18,11 +18,14 @@ describe('sludgy trucker coffee router', function() {
 });
 
 describe('a get request to an invalid route', function() {
+
   it('should respond with a 404', function() {
+
     var req = {
       url: 'this-coffee-tastes-horrible',
       method: 'GET'
     };
+
     var res = {
       writeHead: function(status, headers) {
         expect(status).to.eql(404);
@@ -33,14 +36,71 @@ describe('a get request to an invalid route', function() {
       },
 
       end: function() {
+      }
 
+    };
+
+    Router.route(req, res);
+
+  });
+});
+
+describe('a get request to a valid route using our shorthand .get() with no second argument', function() {
+  before(function(){
+    Router.get('/');
+  });
+
+  it('should write that the user needs to inclde a second argument', function() {
+    var req = {
+      url: '/',
+      method: 'GET'
+    };
+    var res = {
+      writeHead: function(status, headers) {
+        expect(status).to.eql(200);
+        expect(headers).to.eql({"Content-Type": "text/plain"});
+      },
+
+      write: function(text){
+        expect(text).to.eql('oops, looks like you forgot to include a 2nd argument in the .get()');
+      },
+
+      end: function() {
       }
     };
     Router.route(req, res);
-    });
+  });
 });
 
-describe('a get request to a valid route', function() {
+describe('a get request to a valid route using our shorthand .get() with a string for the 2nd parameter instead of a callback', function() {
+  before(function(){
+    Router.get('/', 'this is a string');
+  });
+
+  it('should respond with a 200 status and write a string', function() {
+    var req = {
+      url: '/',
+      method: 'GET'
+    };
+    var res = {
+      writeHead: function(status, headers) {
+        expect(status).to.eql(200);
+        expect(headers).to.eql({"Content-Type": "text/plain"});
+      },
+
+      write: function(text){
+        expect(text).to.eql('this is a string');
+      },
+
+      end: function() {
+      }
+    };
+    Router.route(req, res);
+  });
+});
+
+
+describe('a get request to a valid route with a callback as the 2nd parameter', function() {
   before(function(){
     Router.get('/', function(req, res){
       res.writeHead(200, {'Content-Type' : 'text/html'});
@@ -71,7 +131,32 @@ describe('a get request to a valid route', function() {
   });
 });
 
-describe('a post request to a valid route', function() {
+describe('a post request to a valid route using our shorthand .post() but no second argument', function() {
+  before(function(){
+    Router.post('/coffay');
+    });
+  });
+  it('should respond with a 200 status fo sho', function() {
+    var req = {
+      url: '/coffay',
+      method: 'POST',
+      data: '{"nevergonna":"giveyouup"}'
+    };
+    var res = {
+      writeHead: function(status, headers) {
+        expect(status).to.eql(200);
+        expect(headers).to.eql({'Content-Type': 'application/json'});
+      },
+      write: function(text){
+        expect(text).to.eql('{"nevergonna":"giveyouup"}');
+      },
+      end: function() {
+      }
+    };
+    Router.route(req, res);
+  });
+
+describe('a post request to a valid route with a callback as the 2nd parameter', function() {
   before(function(){
     Router.post('/coffay', function(req, res){
       res.writeHead(200, {"Content-Type": "application/json"});
