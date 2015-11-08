3db53ae1e1b436764a7a9e1b5b0c11c25c1e51ff
Craig Campbell <craig.campbell8@gmail.com>
2015-11-07 16:55:55 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = exports = require(__dirname + '/lib/router.js');
+module.exports = exports = {};
+exports.router = require(__dirname + '/lib/router.js');
+exports.server = require(__dirname + '/lib/server.js');
 
 
 
